657767ae42a290191feb60476bee7c54b6f480c6
bapuAbhijeet <abhijeet15111996@gmail.com>
2019-07-24 13:51:30 +0530

    app commit

--- src/main/java/com/app/restapp1/MainController.java
+++ src/main/java/com/app/restapp1/MainController.java
@@ -2,23 +2,39 @@ package com.app.restapp1;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
 
-	@GetMapping("/Welcome")
+	@GetMapping("/welcome")
 	@ResponseBody
 	public String sayWelcome() {
 		return "Welcome to App";
 	}
+	//must match with jsp page for dependency tomcat-embed-jasper in pom
 	@GetMapping("/login")
-	@ResponseBody
+	//@ResponseBody
 	public String login() {
-		return "Login";
+		return "login";
+		
 	}
 	
-	
+	@PostMapping("/validateUser")
+	//@ResponseBody
+	public String validateUser(
+	@RequestParam("uName")String uName,
+	@RequestParam("pwd")String pwd) {
+		if(uName.equals("a") && pwd.equals("p")){
+			return "success";
+		}
+		else
+		{
+			return "failure";
+		}
+	}
 	
 	
 	
---
+++ src/main/webapp/WEB-INF/mypages/login.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<form action="/validateUser" method="POST" >
+	<input type ="text" name="uName"></input>
+	<input type="password" name="pwd"></input>
+	<button type ="submit">Log in </button>
+</form>
+</body>
+</html>
